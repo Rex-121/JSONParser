4a94920e255409bb8a85df1ee7739a384eb09a60
Tyrant <rex_wzc@163.com>
2022-04-10 01:15:26 +0800

    增加JSON的解析

---
+++ JSONParser/JSON.swift
@@ -0,0 +1,117 @@
+//
+//  JSON.swift
+//  JSONParser
+//
+//  Created by Tyrant on 2022/3/28.
+//
+
+import Foundation
+import CloudKit
+import SwiftUI
+
+struct JSON {
+    
+    enum `Type`: String {
+        case dictionary
+        case array
+        case single
+    }
+    
+    var type: JSON.`Type` {
+        JSON.FindType(data: data)
+    }
+    
+    let key: String
+    
+    let data: Any
+    
+    let index: Int
+    
+    let contains: [JSON]
+    
+    public init(index: Int = -1, key: String = "", _ data: Any) {
+        self.data = data
+        self.key = key
+        self.index = index
+        
+        switch JSON.FindType(data: data) {
+        case .dictionary:
+            
+            let a = data as? Dictionary<String, Any> ?? Dictionary<String, Any>()
+            
+            contains = a.map { JSON(index: index + 1, key: $0, $1) }
+            
+        case .array:
+            let a = data as? Array<Any> ?? []
+            contains = a.map { JSON(index: index + 1, $0) }
+            
+        case .single:
+            contains = []
+        }
+        
+    }
+    
+    //    func allLineViews() -> [JSONKeyValue] {
+    //        let dic = data as? Dictionary<String, Any> ?? [:]
+    //        return dic.ToJSONKeyValue
+    //    }
+}
+
+
+extension JSON: CustomDebugStringConvertible {
+    
+    static func FindType(data: Any) -> JSON.`Type` {
+        switch data {
+        case is Array<Any>:
+            return .array
+        case is Dictionary<String, Any>:
+            return .dictionary
+        default:
+            return .single
+        }
+    }
+    
+    public func adf() {
+        contains.forEach { v in
+            print(v.debugDescription)
+        }
+    }
+    
+    var keyDescription: String {
+        key.isEmpty ? "" : "\(key): "
+    }
+    
+    var debugDescription: String {
+        
+        switch type {
+        case .single:
+            return "\(index.TabString)\(keyDescription)\(data)"
+        case .dictionary:
+            return """
+\(index.TabString)\(keyDescription)
+\(index.TabString){
+\(contains.map { $0.debugDescription }.joined(separator: "\n"))
+\(index.TabString)}
+"""
+        case .array:
+            return """
+\(index.TabString)\(keyDescription)
+\(index.TabString)[
+\(contains.map { $0.debugDescription }.joined(separator: "\n"))
+\(index.TabString)]
+"""
+        }
+        
+        
+    }
+    
+}
+
+
+extension Int {
+    
+    var TabString: String {
+        return Array(repeating: "    ", count: self).joined()
+    }
+    
+}
---
+++ JSONParser/JSONValidate.swift
@@ -0,0 +1,41 @@
+//
+//  JSONValidate.swift
+//  JSONParser
+//
+//  Created by Tyrant on 2022/4/9.
+//
+
+import Foundation
+
+
+extension String {
+    
+    
+    private var JSONReadingOptions: JSONSerialization.ReadingOptions {
+        if #available(iOS 15.0, *) {
+            return first == "[" ? [.json5Allowed] : [.topLevelDictionaryAssumed, .json5Allowed]
+        } else {
+            return [.fragmentsAllowed]
+        }
+    }
+    
+    var isValidateJSON: Result<JSON, Error> {
+        
+        if (isEmpty) { return .success(JSON("")) }
+        
+        do {
+            let data = try JSONSerialization
+                .jsonObject(with: data(using: .utf8)!,
+                            options: JSONReadingOptions)
+            
+            return .success(JSON(data))
+        }
+        catch {
+            print(error)
+            return .failure(error)
+        }
+        
+        
+    }
+    
+}
